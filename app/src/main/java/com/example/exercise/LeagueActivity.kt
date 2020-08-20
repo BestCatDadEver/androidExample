7813a9aaacc6a95c18e966868e085c2b8ff01817
Carlos <cgelpi1439@gmail.com>
2020-08-20 13:57:07 -0300

    Added skill layout

--- app/src/main/java/com/example/exercise/LeagueActivity.kt
+++ app/src/main/java/com/example/exercise/LeagueActivity.kt
@@ -1,11 +1,18 @@
 package com.example.exercise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+        btn_next.setOnClickListener {
+            val skill = Intent(this, SkillActivity::class.java)
+            startActivity(skill)
+        }
     }
 }
